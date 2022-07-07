7d63a96d313c0b401b966a14c2787d1891d56427
jaimebriesemeister <82653221+jaimebriesemeister@users.noreply.github.com>
2022-07-07 10:35:36 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+#leia o arquivo csv
+df = pd.read_csv("final.csv")
+#verifique o número de linhas e de colunas
+print(df.shape)
+
+# Exclua as colunas
+
+#Renomeie os cabeçalhos das colunas
+
+#Save os dados no arquivo main.csv
